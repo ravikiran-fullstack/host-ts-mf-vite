7d13a4e78ff2715cdbfeedcc6114e6b72d3fe649
ravikiran <ravikiranjobs21@gmail.com>
2024-07-26 15:07:59 -0400

    working vite ts module-federation host with simple button imported from remote

---
+++ module.d.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+
+declare module "sharedComp/Button" {
+	const CounterAppOne: React.ComponentType;
+
+	export default Button;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: "host",
+      remotes: {
+        app: "http://localhost:5001/assets/remoteEntry.js",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      external: ['app/Button'] // Add this line
+    },
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
