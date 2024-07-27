0fcfb32f467a9b13b122aac81534f0e7c32ddbec
ravikiran <ravikiranjobs21@gmail.com>
2024-07-27 00:20:55 -0400

    imported second component i.e, ProcessAndCharts from remote MFE

---
+++ @mf-types/app/ProcessAndCharts.d.ts
@@ -0,0 +1,2 @@
+export * from './_types/ProcessAndCharts';
+export { default } from './_types/ProcessAndCharts';
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import './App.css'
 
 import RemoteButtonType from '../@mf-types/app/Button';
+import RemoteProcessAndChartsType from '../@mf-types/app/ProcessAndCharts';
 import React from 'react';
 
 const RemoteButton = React.lazy(() => import('app/Button')) as unknown as typeof RemoteButtonType;
+const RemoteProcessAndCharts = React.lazy(() => import('app/ProcessAndCharts')) as unknown as typeof RemoteProcessAndChartsType;
 
 function App() {
 
   return (
     <>
+    <div>
+      <h1>Host App</h1>
+    </div>
     <RemoteButton size="large"/>
+    <RemoteProcessAndCharts/>
     </>
   )
 }
